c91e8f2c3c60b509180c7d98572d2a77864f83b4
GRADEN BISHOP <GRADEN.BISHOP2995@OMAIL.OTECH.EDU>
2022-02-16 11:06:47 -0700

    first commit

---
+++ 4 practice/Account.cs
@@ -0,0 +1,17 @@
+﻿namespace _4_practice
+{
+    class Account
+    {
+        private string name;
+
+        public void SetName(string accountName)
+        {
+            name = accountName;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+    }
+}
---
+++ 4 practice/AccountTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _4_practice
+{
+    class AccountTest
+    {
+        static void Main()
+        {
+            Account myAccount = new Account();
+
+            Console.WriteLine($"Initial name is: {myAccount.GetName()}");
+
+            Console.Write("Enter the name: ");
+            string theName = Console.ReadLine();
+            myAccount.SetName(theName);
+
+            Console.WriteLine($"myAccount's name is: {myAccount.GetName()}");
+        }
+    }
+}
