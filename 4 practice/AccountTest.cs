9cc0aeb30cde2ee13190970fce633fe50e3af4d2
GRADEN BISHOP <GRADEN.BISHOP2995@OMAIL.OTECH.EDU>
2022-02-18 13:51:58 -0700

    complete

--- 4 practice/AccountTest.cs
+++ 4 practice/AccountTest.cs
@@ -9,15 +9,22 @@ namespace _4_practice
             Account account1 = new Account("Jane Green", 50.00m);
             Account account2 = new Account("John Blue", -7.53m);
 
-            Console.WriteLine($"{account1.Name}'s balance : {account1.Balance:C}");
-            Console.WriteLine($"{account2.Name}'s balance : {account2.Balance:C}");
+            Console.WriteLine($"{account1.Name}'s balance : {account1.Balance:c}");
+            Console.WriteLine($"{account2.Name}'s balance : {account2.Balance:c}");
 
             Console.Write("\nEnter deposit amount for account1: ");
             decimal depositAmount = decimal.Parse(Console.ReadLine());
-            Console.WriteLine($"adding {depositAmount:C} to account1 valance\n");
+            Console.WriteLine($"adding {depositAmount:c} to account1 valance\n");
             account1.Deposit(depositAmount);
 
             Console.WriteLine($"{account1.Name}'s balance: {account1.Balance:C}");
+            Console.WriteLine($"{account2.Name}'s balance: {account2.Balance:C}");
+            Console.Write("\nEnter deposit amount for account2: ");
+            depositAmount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine($"adding {depositAmount:c} to account2's balance");
+            account2.Deposit(depositAmount);
+
+            Console.WriteLine($"{account1.Name}'s balance: {account1.Balance:c}");
             Console.WriteLine($"{account2.Name}'s balance: {account2.Balance:C}");
         }
     }
