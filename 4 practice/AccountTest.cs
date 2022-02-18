57123a451a982a0a73f1a3c57bac5fe84dfae74c
GRADEN BISHOP <GRADEN.BISHOP2995@OMAIL.OTECH.EDU>
2022-02-18 11:51:45 -0700

    update 2

--- 4 practice/AccountTest.cs
+++ 4 practice/AccountTest.cs
@@ -6,11 +6,19 @@ namespace _4_practice
     {
         static void Main()
         {
-            Account account1 = new Account("Jane Green");
-            Account account2 = new Account("John Blue");
+            Account account1 = new Account("Jane Green", 50.00m);
+            Account account2 = new Account("John Blue", -7.53m);
 
-            Console.WriteLine($"account 1 name is: {account1.Name}");
-            Console.WriteLine($"account 2 name is: {account2.Name}");
+            Console.WriteLine($"{account1.Name}'s balance : {account1.Balance:C}");
+            Console.WriteLine($"{account2.Name}'s balance : {account2.Balance:C}");
+
+            Console.Write("\nEnter deposit amount for account1: ");
+            decimal depositAmount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine($"adding {depositAmount:C} to account1 valance\n");
+            account1.Deposit(depositAmount);
+
+            Console.WriteLine($"{account1.Name}'s balance: {account1.Balance:C}");
+            Console.WriteLine($"{account2.Name}'s balance: {account2.Balance:C}");
         }
     }
 }
